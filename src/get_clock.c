33d750deaaa4d1faf5b698bad12502afff4c2e29
smiller <gshanemiller6@gmail.com>
2023-03-13 19:49:10 +0000

    SP

--- src/get_clock.c
+++ src/get_clock.c
@@ -64,71 +64,7 @@ http://en.wikipedia.org/wiki/Linear_regression#Parameter_estimation
 */
 static double sample_get_cpu_mhz(void)
 {
-	struct timeval tv1, tv2;
-	double sx = 0, sy = 0, sxx = 0, syy = 0, sxy = 0;
-	int i;
-
-	/* Regression: y = a + b x */
-	long x[MEASUREMENTS];
-	cycles_t y[MEASUREMENTS];
-	double a; /* system call overhead in cycles */
-	double b; /* cycles per microsecond */
-	double r_2;
-
-	for (i = 0; i < MEASUREMENTS; ++i) {
-		cycles_t start = get_cycles();
-
-		if (gettimeofday(&tv1, NULL)) {
-			fprintf(stderr, "gettimeofday failed.\n");
-			return 0;
-		}
-
-		do {
-			if (gettimeofday(&tv2, NULL)) {
-				fprintf(stderr, "gettimeofday failed.\n");
-				return 0;
-			}
-		} while ((tv2.tv_sec - tv1.tv_sec) * 1000000 +
-				(tv2.tv_usec - tv1.tv_usec) < USECSTART + i * USECSTEP);
-
-		x[i] = (tv2.tv_sec - tv1.tv_sec) * 1000000 +
-			tv2.tv_usec - tv1.tv_usec;
-		y[i] = get_cycles() - start;
-		if (DEBUG_DATA)
-			fprintf(stderr, "x=%ld y=%Ld\n", x[i], (long long)y[i]);
-	}
-
-	for (i = 0; i < MEASUREMENTS; ++i) {
-		double tx = x[i];
-		double ty = y[i];
-		sx += tx;
-		sy += ty;
-		sxx += tx * tx;
-		syy += ty * ty;
-		sxy += tx * ty;
-	}
-
-	b = (MEASUREMENTS * sxy - sx * sy) / (MEASUREMENTS * sxx - sx * sx);
-	a = (sy - b * sx) / MEASUREMENTS;
-
-	if (DEBUG)
-		fprintf(stderr, "a = %g\n", a);
-	if (DEBUG)
-		fprintf(stderr, "b = %g\n", b);
-	if (DEBUG)
-		fprintf(stderr, "a / b = %g\n", a / b);
-	r_2 = (MEASUREMENTS * sxy - sx * sy) * (MEASUREMENTS * sxy - sx * sy) /
-		(MEASUREMENTS * sxx - sx * sx) /
-		(MEASUREMENTS * syy - sy * sy);
-
-	if (DEBUG)
-		fprintf(stderr, "r^2 = %g\n", r_2);
-	if (r_2 < 0.9) {
-		fprintf(stderr,"Correlation coefficient r^2: %g < 0.9\n", r_2);
-		return 0;
-	}
-
-	return b;
+  return 2801.000000;
 }
 
 #if !defined(__s390x__) && !defined(__s390__)
@@ -210,30 +146,7 @@ static double proc_get_cpu_mhz(int no_cpu_freq_warn)
 
 double get_cpu_mhz(int no_cpu_freq_warn)
 {
-	#if defined(__s390x__) || defined(__s390__)
-	return sample_get_cpu_mhz();
-	#else
-	double sample, proc, delta;
-	sample = sample_get_cpu_mhz();
-	proc = proc_get_cpu_mhz(no_cpu_freq_warn);
-	#ifdef __aarch64__
-	if (proc < 1)
-		proc = sample;
-	#endif
-	#ifdef __riscv
-	if (proc <= 0)
-		proc = sample;
-	#endif
-
-	if (!proc || !sample)
-		return 0;
-
-	delta = proc > sample ? proc - sample : sample - proc;
-	if (delta / proc > 0.02) {
-		return sample;
-	}
-	return proc;
-#endif
+  return sample_get_cpu_mhz();
 }
 
 #if defined(__riscv)
